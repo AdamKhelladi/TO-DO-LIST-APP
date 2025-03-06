abc24d13b7722d8688a36c6e780cd28c49026485
AdamTech <147929248+AdamKhelladi@users.noreply.github.com>
2025-03-06 12:47:18 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let theInput = document.querySelector(".add-task input");
 let addBtn = document.querySelector(".add-task .plus");
+
 let tasksContainer = document.querySelector(".tasks-content");
 let boxes = document.querySelector(".boxes");
 let noTasksMsg = document.querySelector(".no-tasks");
